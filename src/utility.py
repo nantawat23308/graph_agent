062b22d0e6ecfcd3a323ac001b6de1947a38bcd3
nantawats <nantawat23308@gmail.com>
2025-12-15 21:57:04 +0700

    init

---
+++ src/utility.py
@@ -0,0 +1,51 @@
+from langchain.messages import ToolMessage
+from langchain.tools import tool
+from src.state import MessagesState
+
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiply `a` and `b`.
+
+    Args:
+        a: First int
+        b: Second int
+    """
+    return a * b
+
+@tool
+def add(a: int, b: int) -> int:
+    """Adds `a` and `b`.
+
+    Args:
+        a: First int
+        b: Second int
+    """
+    return a + b
+
+@tool
+def divide(a: int, b: int) -> float:
+    """Divide `a` and `b`.
+
+    Args:
+        a: First int
+        b: Second int
+    """
+    return a / b
+
+def bind_toolchain_model(state: MessagesState):
+
+    # Augment the LLM with tools
+    tools = [add, multiply, divide]
+    tools_by_name = {tool.name: tool for tool in tools}
+    return tools_by_name
+    model_with_tools = model.bind_tools(tools)
+
+def tool_node(state: dict):
+    """Performs the tool call"""
+
+    result = []
+    for tool_call in state["messages"][-1].tool_calls:
+        tool = tools_by_name[tool_call["name"]]
+        observation = tool.invoke(tool_call["args"])
+        result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
+    return {"messages": result}
